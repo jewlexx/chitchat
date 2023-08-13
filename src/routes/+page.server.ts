a1788831f4724fff2e7f451b13972bcf9974c9a1
Juliette Cordor <juliette@jewelexx.com>
2023-08-14 09:44:01 +1000

    fixed formatting

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
-  import { supabase } from "$lib";
+import { supabase } from "$lib";
 
-  export async function load() {
-    const { data } = await supabase.from("messages").select();
-    return {
-      messages: data ?? [],
-    };
-  }
+export async function load() {
+  const { data } = await supabase.from("messages").select();
+  return {
+    messages: data ?? [],
+  };
+}
