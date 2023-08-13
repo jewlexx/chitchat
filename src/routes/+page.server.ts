f85cd151028df5894aa6e0a614cbe32a602d86a1
Juliette Cordor <juliette@jewelexx.com>
2023-08-14 09:39:17 +1000

    updates and fixes

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,8 @@
+  import { supabase } from "$lib";
+
+  export async function load() {
+    const { data } = await supabase.from("messages").select();
+    return {
+      messages: data ?? [],
+    };
+  }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+</script>
+
+<ul>
+	{#each data.messages as message}
+		<li>{message}</li>
+	{/each}
+</ul>
