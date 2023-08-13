e7bb7edf30a39eccc742eefb00e9b61945f60edb
Juliette Cordor <juliette@jewelexx.com>
2023-08-14 09:34:12 +1000

    use static env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-import { env } from '$env/dynamic/public';
-
-const { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANONKEY } = env;
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANONKEY } from '$env/static/public';
 
 if (PUBLIC_SUPABASE_URL === undefined || PUBLIC_SUPABASE_ANONKEY === undefined) {
     throw new Error("Missing Supabase url or key");
