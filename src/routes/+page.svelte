e53a231fe1e5c7ac00b67bdc24d4b091b1852139
Juliette Cordor <juliette@jewelexx.com>
2023-08-14 10:34:58 +1000

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script>
 	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	let messages = supabase.from('messages').select().then(console.log);
 </script>
 
 <ul>
-	{#each data.messages as message}
+	<!-- {#each messages as message}
 		<li>{message}</li>
-	{/each}
+	{/each} -->
 </ul>
