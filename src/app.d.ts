f3545bb0d60a687b87b1b4901a1f6ab71561bd42
Juliette Cordor <juliette@jewelexx.com>
2023-08-14 10:17:15 +1000

    fixed types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,20 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { GenericSchema } from '@supabase/supabase-js/dist/module/lib/types';
+
+type Database = any;
+type SchemaName = {} extends string & 'public' & keyof Database ? 'public' : string & keyof Database;
+type Schema = {} & GenericSchema;
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			supabase: SupabaseClient<Database, SchemaName, Schema>,
+			getSession: () => Promise<any>
+
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,9 +1,9 @@
 
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANONKEY } from '$env/static/public';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
-if (PUBLIC_SUPABASE_URL === undefined || PUBLIC_SUPABASE_ANONKEY === undefined) {
+if (PUBLIC_SUPABASE_URL === undefined || PUBLIC_SUPABASE_ANON_KEY === undefined) {
     throw new Error("Missing Supabase url or key");
 }
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANONKEY)
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
